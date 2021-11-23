225c4d54a945df4e67e253c94b861d055696a3a3
Rui Coutinho <ruca003@gmai.com>
2021-11-23 08:12:51 +0000

    ficha5 acabada

--- ficha5/prob6.c
+++ ficha5/prob6.c
@@ -5,22 +5,22 @@
 
 int get_bit(int num,int pos){
     
-    if (pos == 1)
-        return (num%(int)pow(10,pos));
+    if (pos == 0)
+        return (num%(int)pow(10,pos+1));
     else
-        return (num%(int)pow(10,pos))/pow(10,pos-1);
+        return (num%(int)pow(10,pos+1))/pow(10,pos);
 
 }
 
 int calc_si(int a, int b, int cin){
     
-    return (a ^ b ^ cin);  // usa-se o XOR pois 1 XOR 0 = 1   e   1 XOR 1 = 0
+    return ((a ^ b) ^ cin);  // usa-se o XOR pois 1 XOR 0 = 1   e   1 XOR 1 = 0
 
 }
 
 int calc_cin(int a, int b, int cin){
     
-    return (((a | b) & cin)  | (a & b));
+    return (((a ^ b) & cin)  | (a & b));
 
 }
 
@@ -32,20 +32,21 @@ int soma_binario(int a, int b){
 
         ai = get_bit(a,i);
         bi = get_bit(b,i);
-
+        printf("a:%d \t b:%d\n",ai,bi);
         si = calc_si(ai,bi,ci);
-        ci = calc_cin(a,b,ci);
+        ci = calc_cin(ai,bi,ci);
 
-        sum = si * pow(10,i);
-
+        sum += si * pow(10,i);
+        printf("si:%d\t ci:%d\n",si,ci);
         i++;
         resta = a/pow(10,i);
         restb = b/pow(10,i);
+        printf("%d__%d:::\n",resta,restb);
     }
-    while ((resta > 0) || (restb > 0));
+    while ((resta > 0) || (restb > 0) || (ci > 0));
     
     
-    return 0;
+    return sum;
 }
 
 
---
+++ ficha5/prob7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+float rando(){
+    
+    return rand()/(float)RAND_MAX;
+
+}
+
+
+int main(){
+
+    int m=0, n,i=0;
+    float x,y;
+
+    printf("Numero lancamentos? ");
+    scanf("%d", &n);
+
+    for(i=1;i<n+1;i++)
+    {
+        x=rando();
+        y=rando();
+
+        if ((x*x + y*y) < 1)
+            m++;
+        
+        printf("pi: %f\n",4*m*1.0/i);
+    }   
+    
+
+}
